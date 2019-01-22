d8878cdb9dbbbe1048dc400a04003a6c983dbf47
Apoorv Karmalkar <apoorvkarmalkar8111995@gmail.com>
2019-01-22 06:40:14 +0000

    modified:   app.js 	modified:   css/navbar.css 	new file:   css/reg-success-css.css 	modified:   views/index.ejs 	modified:   views/movie-database.ejs 	modified:   views/movies.ejs 	new file:   views/regsuccess.ejs

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var mysql=require("mysql");
 var urlencodedParser = bodyparser.urlencoded({ extended: false });
 var request=require("request");
 var session=require("express-session");
+var bcrypt=require("bcrypt");
 app.use(session({secret: 'ssshhh', resave: 'true', saveUninitialized: 'true'}));
 app.use(express.static("css"));
 app.use(express.static("js"));
@@ -15,8 +16,9 @@ var pool=mysql.createPool({
     password:"",
     database:"c9"
 });
-var sess;
+var sess,sess2;
 app.get("/",function(req,res){
+   sess2=req.session;
 pool.getConnection(function(err,con){
    if(err) throw err;
    console.log("Connection Established");
@@ -25,15 +27,27 @@ pool.getConnection(function(err,con){
       con.release();
       if(err) throw err;
       console.log(result);
+      if(!sess2){
+         
       res.render("index.ejs",{resultVar:result[0].poster,
             resultVar2:result[1].poster,
             resultVar3:result[2].poster
          });
+      }
+      else {
+         res.render("index.ejs",{resultVar:result[0].poster,
+            resultVar2:result[1].poster,
+            resultVar3:result[2].poster,
+            sessLogin:sess2.name
+         });
+      }
+      
       }); 
    });
 });
 app.post("/:city",urlencodedParser,function(req,res){
       sess=req.session;
+      sess2=req.session;
       var cty=req.body.city;
       sess.city=req.body.city;
       var sql="select poster from tbl_movies where city=? order by id desc limit 3";
@@ -43,7 +57,8 @@ app.post("/:city",urlencodedParser,function(req,res){
          res.render("index.ejs",{
          resultVar:result[0].poster,
          resultVar2:result[1].poster,
-         resultVar3:result[2].poster
+         resultVar3:result[2].poster,
+         sessLogin:sess2.name
          });
       });
    });
@@ -63,13 +78,60 @@ app.get("/search",function(req,res){
 });
 app.get("/:city/movies",function(req, res) {
    sess=req.session;
+   sess2=req.session;
    var sql="select poster from tbl_movies where city=? order by id desc";
    pool.query(sql,[sess.city],function(err,result){
       if (err) throw err;
       console.log(result);
-      res.render("movies.ejs",{resultVar:result})
+      res.render("movies.ejs",{resultVar:result,sessLogin:sess2.name});
    });
 });
+app.post("/user/signup",urlencodedParser,function(req,res){
+   var fname=req.body.fname;
+   var lname=req.body.lname;
+   var mobile=req.body.mobile;
+   var email=req.body.email;
+   var pass=req.body.password;
+      bcrypt.hash(pass,10,function(err,hash){
+         var sql="insert into tbl_users(first_name,last_name,mobile,email,password) values('"+fname+"','"+lname+"','"+mobile+"','"+email+"','"+hash+"')";
+         pool.query(sql,function(err, result) {
+         if (err) throw err;
+         console.log("Records inserted");
+         res.render("regsuccess.ejs");
+         });
+      });
+});
+app.post("/user/login",urlencodedParser,function(req,res){
+   var mobile=req.body.mobile;
+   var pass=req.body.password;
+   var hash="select password from tbl_users where mobile='"+mobile+"'";
+   sess2=req.session;
+   pool.query(hash,function(err1, hresult) {
+      if(err1) throw err1;
+      hresult = hresult[0].password;
+      console.log(hresult);
+         bcrypt.compare(pass,hresult,function(error,results){
+            console.log(results);
+      if(results==true){
+         var sql="select first_name from tbl_users where mobile='"+mobile+"' and password='"+hresult+"'";
+         pool.query(sql,function(err2, result) {
+         if(err2) throw err2;
+         console.log(result);
+         if(result.length==1){
+         console.log("Login Successful");
+         sess2.name=result[0].first_name;
+         res.redirect("/");
+         }
+         });
+      }
+      else
+      {
+         console.log("Login Failed");
+      }
+   });
+   });
+}); 
+
 app.listen(process.env.PORT,process.env.IP,function(){
    console.log("Server Started"); 
 });
